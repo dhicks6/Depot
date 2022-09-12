9a79cf2f2c152d0dd326a58f567ef2feff259def
dhicks6 <dhicks23@msudenver.edu>
2022-09-12 15:08:07 -0600

    Fixed products_controller_test.rb to create a random book title

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-   
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -11,7 +11,25 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:image_url].any?
   end
  
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product is not valid without a unique title" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy",
+                          price:       1,
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title:       products(:ruby).title,
+                          description: "yyy",
+                          price:       1,
+                          image_url:   "fred.gif")
+
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
+  end
+
 end
